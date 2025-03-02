b8bcc430a6e98c9d0c2b96f74c0c5fef76bfbc96
Pavan Eega <pavaneega123@gmail.com>
2025-03-03 00:15:56 +0530

    updated

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,redirect,url_for,request,render_template
+'''from flask import Flask,redirect,url_for,request,render_template
 app = Flask(__name__)
 
 @app.route('/api/data')
@@ -9,3 +9,16 @@ def home():
 if __name__ == '__main__':
     app.run(debug = True)
 
+'''
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
